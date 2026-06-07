d90108509e733f3baf8d57c5940bc7aa2247d6d8
Saijee Itokar <saijeeitokar509@gmail.com>
2026-06-07 17:18:08 +0530

    Add files via upload

---
+++ Loops/Question_3.cpp
@@ -0,0 +1,10 @@
+// Average of Three
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,avg;
+    cout<<"Enter value of a,b,c:";
+    cin>>a>>b>>c;
+    avg=(a+b+c)/3;
+    cout<<" Average of Three:"<<avg;
+}
