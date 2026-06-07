9812a58bdf68301a5277908a0612bc77b64194d4
Saijee Itokar <saijeeitokar509@gmail.com>
2026-06-07 17:20:32 +0530

    Delete Loops/Question_2.cpp

--- Loops/Question_2.cpp
+++
@@ -1,9 +0,0 @@
-//  Area of a Rectangle
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b;
-    cout<<"Enter length And width:";
-    cin>>a>>b;
-    cout<<"area of rectangle:"<<a*b;
-}
