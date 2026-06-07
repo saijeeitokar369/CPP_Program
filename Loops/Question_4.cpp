7ebc12e72aeb8210222b9bba5f645c5bbee2b44d
Saijee Itokar <saijeeitokar509@gmail.com>
2026-06-07 17:20:01 +0530

    Delete Loops/Question_4.cpp

--- Loops/Question_4.cpp
+++
@@ -1,9 +0,0 @@
-// Data Type Sizes
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Size of double:"<<sizeof(double)<<"byte(s)"<<"\n";
-    cout<<"Size of float:"<<sizeof(float)<<"byte(s)"<<"\n";
-    cout<<"Size of int:"<<sizeof(int)<<"byte(s)"<<"\n";
-    cout<<"Size of char:"<<sizeof(char)<<"byte(s)";
-}
